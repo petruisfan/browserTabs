fae4329cc3ba954063bbe64f860bfa1293d59c09
Petru Isfan <petru.isfan@gmail.com>
2014-07-26 01:27:20 +0300

    Added dashboard image

--- src/association/AssociationService.js
+++ src/association/AssociationService.js
@@ -3,7 +3,9 @@ app.factory("AssociationService", function() {
     var _data = [
         {id: 1, name: "Association1"},
         {id: 2, name: "Association2"},
-        {id: 3, name: "Association3"}
+        {id: 3, name: "Association3"},
+        {id: 4, name: "Association4"},
+        {id: 5, name: "Association5"}
     ]
 
     return {
